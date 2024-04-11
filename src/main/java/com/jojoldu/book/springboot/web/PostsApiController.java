f6249e87762b0b8263570b7fdf2eb79274244083
z1 <jjo_6@naver.com>
2024-04-11 21:18:27 +0900

    3. jpa 적용 및 등록 api 만들기

---
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    //등록하기
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+
+}
