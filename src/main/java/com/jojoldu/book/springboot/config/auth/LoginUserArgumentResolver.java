60e800f08edf927db48c07b3f25816127336ab7d
z1 <jjo_6@naver.com>
2024-05-26 18:44:26 +0900

    5.4 어노테이션 기반으로 개선하기

--- src/main/java/com/jojoldu/book/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -11,8 +11,8 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpSession;
 
-@RequiredArgsConstructor
-@Component
+@RequiredArgsConstructor    // 생성자를 자동으로 생성
+@Component                  // 타입기반 자동 주입 어노테이션 = @Autowired
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final HttpSession httpSession;
@@ -21,7 +21,9 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     //컨트롤러 메서드의 특정 파라미터를 지원하는지 판단.
     @Override
     public boolean supportsParameter(MethodParameter parameter){
-        boolean isLoginUserAnnotation = parameter.getParameterAnnotations(LoginUser.class) != null;
+        // 어노테이션이 파라미터값에 위치하는지 확인
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        // 파라미터 클래스 타입이 SessionUser.class 인지 확인
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
 
         return isLoginUserAnnotation && isUserClass;
